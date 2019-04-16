464ab1c7830ccc183b783cd70390b806b87b437c
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2019-04-16 08:59:04 +0200

    Recherche sur une API

--- src/App.js
+++ src/App.js
@@ -3,23 +3,67 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: "",
+      users: [],
+      selectedUser: null
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.startSearch = this.startSearch.bind(this);
+    this.selectUser = this.selectUser.bind(this);
+  }
+
+  handleChange(event) {
+    const { value } = event.target;
+    this.setState({
+      search: value
+    });
+  }
+
+  startSearch() {
+    fetch(`https://searchapi.jsx.fr/?search=${this.state.search}`)
+      .then(res => res.json())
+      .then(users => {
+        console.log(users);
+        this.setState({
+          users: users
+        });
+      });
+  }
+
+  selectUser(index) {
+    this.setState({
+      selectedUser: this.state.users[index]
+    })
+  }
+
   render() {
+    const selected = this.state.selectedUser;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input name="search" type="text" onChange={this.handleChange} />
+        <button onClick={this.startSearch}>
+          Search
+        </button>
+        <ul>
+          {
+            this.state.users.map((user, index) => (
+              <li 
+                key={user.login.uuid}
+                onClick={() => this.selectUser(index)}
+              >
+                {user.name.first} {user.name.last}
+              </li>
+            ))
+          }
+        </ul>
+        {
+          selected && (
+            <img src={selected.picture.large} />
+          )
+        }
       </div>
     );
   }
